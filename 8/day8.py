3cb2f4ff5007d8fa697d37fcd5eacd07b65faec3
Nip403 <41174986+nip403@users.noreply.github.com>
2022-12-14 05:57:59 +0000

    Update day8.py

--- 8/day8.py
+++ 8/day8.py
@@ -131,5 +131,5 @@ def part2():
     return max(max(tree[1] for tree in row) for row in trees)
 
 if __name__ == "__main__":
-    print("Part 2:", part1())
+    print("Part 1:", part1())
     print("Part 2:", part2())
